33cb02d37b40406d4bcdf33ba0cacf567b86a28f
Misbah yousaf <107263622+Misbahkk@users.noreply.github.com>
2024-09-26 22:37:12 +0500

    Add files via upload

---
+++ day_02/conversionVariable.js
@@ -0,0 +1,30 @@
+let score = "33";
+let score2 = "441bc";
+let player = "John";
+let notPlayer = true
+let numbers = undefined
+let goal = null
+
+
+let changedtatype1 = Number(score)
+let changedtatype2 = Number(score2)
+let changedtatype3 = Number(player)
+let changedtatype4 = Number(notPlayer)
+let changedtatype5 = Number(numbers)
+let changedtatype6 = Number(goal)
+
+console.table([changedtatype1,changedtatype2,changedtatype3,changedtatype4,changedtatype5,changedtatype6])
+
+
+let stringValue = "misbah"
+let numbervalue = 123
+let zero = 0
+let nullnum = ""
+
+let changedboolen1 = Boolean(stringValue)
+let changedboolen2 = Boolean(numbervalue)
+let changedboolen3 = Boolean(zero)
+let changedboolen4 = Boolean(nullnum)
+
+console.table([changedboolen1,changedboolen2,changedboolen3,changedboolen4])
+
---
+++ day_02/dataType.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+const accountit = 12345
+let email = "misbah@gmaill.com"
+var password = "1234"
+
+
+// Number => 2 to power 53
+// BigInt
+// String => ""
+// boolean => true/flase
+// null => standalone value
+// undefiend => value don't assign
+// Symbol => unique and immutable
+
+// the type of null is object
+// object
+
+
+console.table([typeof null , typeof undefined, typeof 1234])    
