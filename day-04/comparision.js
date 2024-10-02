0e3c39d333c63299076bcdd983c2e43503d2262e
Misbah yousaf <107263622+Misbahkk@users.noreply.github.com>
2024-10-02 11:30:15 +0000

    Day-04

---
+++ day-04/comparision.js
@@ -0,0 +1,20 @@
+// console.log("2">1);
+// console.log("2" ==2);
+
+
+// strickly check
+// console.log("2" === 2);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+
+
+// This type comparesion alwyas get confused you
